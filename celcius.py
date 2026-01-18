78b371846ea767f8589a14d129379cf598546642
Yashodha2006 <yashodhabasarikatti21@gmail.com>
2026-01-18 12:00:08 +0530

    Update celcius.py

--- celcius.py
+++ celcius.py
@@ -1,9 +1,10 @@
 def celsius_to_fahrenheit(c=0):
-    return (c * 9/5) + 32
+    return (c * 9 / 5) + 32
+
 
 if __name__ == "__main__":
     user_input = input("Enter Celsius (press Enter for default 0): ")
-    if user_input == "":
+    if user_input.strip() == "":
         print(celsius_to_fahrenheit())
     else:
         print(celsius_to_fahrenheit(float(user_input)))
