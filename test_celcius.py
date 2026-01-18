b37cbb36c6c3ed64b5d90821400ff6df8f84b1da
Yashodha2006 <yashodhabasarikatti21@gmail.com>
2026-01-18 11:59:34 +0530

    Update test_celcius.py

--- test_celcius.py
+++ test_celcius.py
@@ -1,10 +1,13 @@
-from celcius import celsius_to_fahrenheit
+from celsius import celcius_to_fahrenheit
+
 
 def test_zero():
     assert celsius_to_fahrenheit() == 32
 
+
 def test_positive():
     assert celsius_to_fahrenheit(100) == 212
+
 
 def test_negative():
     assert celsius_to_fahrenheit(-40) == -40
